43cffbb2dda6a4d6c950bc3d61afc4638a38ef28
Dazka Project <thisishiter@gmail.com>
2025-06-02 01:55:22 +0300

    SceneMenegment+Types

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
     public GameObject main_panel;
     public GameObject ex_panel;
     public GameObject songs_panel;
+
+    public Param param;
+
     private void Start()
     {
         main_panel.SetActive(true);
@@ -32,5 +36,23 @@ public class Menu : MonoBehaviour
         main_panel.SetActive(true);
         ex_panel.SetActive(false);
         songs_panel.SetActive(false);
+    }
+
+    public void loadEx1()
+    {
+        param.type = "ex1";
+        SceneManager.LoadScene(1);
+    }
+
+    public void loadEx2()
+    {
+        param.type = "ex2";
+        SceneManager.LoadScene(1);
+    }
+
+    public void loadLAcords()
+    {
+        param.type = "lAcords";
+        SceneManager.LoadScene(1);
     }
 }
--- Assets/Scripts/Play/PlayMain.cs
+++ Assets/Scripts/Play/PlayMain.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayMain : MonoBehaviour
 {
-    GameObject param;
+    
+    public Metronome met;
+
+    Animator animator;
+    Param param;
 
     void Start()
     {
-        param = GameObject.FindWithTag("Param");
+        animator = GetComponent<Animator>();
+        param = GameObject.FindWithTag("Param").GetComponent<Param>();
         if (param != null)
         {
-            Debug.Log("Знайдено об'єкт з тегом 'param': " + param.name);
-            // Тут ви можете працювати з знайденим об'єктом
+            Debug.Log("Знайдено об'єкт з тегом 'param'");
+            switch (param.type)
+            {
+                case "lAcords":
+                    //met.bpm = 60.0f;
+                    animator.SetInteger("i", 1);
+                    break;
+                case "ex1":
+                    animator.SetInteger("i", 2);
+                    Debug.Log("Виконано");
+                    break;
+                case "ex2":
+                    animator.SetInteger("i", 1);
+                    break;
+                case "STab":
+                    animator.SetInteger("i", 1);
+                    break;
+                case "SFight":
+                    animator.SetInteger("i", 0);
+                    break;
+            }
+            param = null;
+            Destroy(GameObject.FindWithTag("Param"));
         }
         else
         {
@@ -23,6 +50,9 @@ public class PlayMain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
