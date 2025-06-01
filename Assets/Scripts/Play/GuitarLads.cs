f79c9e8d17a3ed118ca82b7b5403d7416e1d10b5
Dazka Project <thisishiter@gmail.com>
2025-06-01 13:38:47 +0300

    lads

---
+++ Assets/Scripts/Play/GuitarLads.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuitarLads : MonoBehaviour
+{
+    public List<GameObject> raw1;
+    public List<GameObject> raw2;
+    public List<GameObject> raw3;
+    public List<GameObject> raw4;
+    public List<GameObject> raw5;
+    public List<GameObject> raw6;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
